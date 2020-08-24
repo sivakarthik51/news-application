df1472c3c3ba0001216841a17403bc1210bef563
Siva Karthik <siramesh@akamai.com>
2020-08-24 23:20:40 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ useState , useEffect } from 'react';
+import NewsCards from './Components/NewsCards/NewsCards';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import wordsToNumbers  from 'words-to-numbers';
+import useStyles from './styles';
 import './App.css';
 
-function App() {
+const alanKey = 'e56f04daa3dfa120b4d752108582f9842e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+
+  const [newsArticles,setNewsArticles] = useState([]);
+  const [activeArticle,setActiveArticle] = useState(-1);
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key:alanKey,
+      onCommand: ({ command , articles , number }) =>{
+        if(command === 'newHeadlines' ) {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        }
+        else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle+1 );
+        }
+        else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers(number, {fuzzy:true}) : number;
+          const article = articles[parsedNumber-1];
+          if(parsedNumber > 20) {
+            alanBtn.playText('Article Number Exceeded');
+          }
+          else if (article){
+            window.open(articles[parsedNumber].url,'_blank');
+            alanBtn().playText('Opening Article');
+          }
+        }
+      }
+    });
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={classes.logoContainer}>
+        <img src={require('./NewsHub.png')} className={classes.logo} alt="News Hub Logo"/>
+
+      </div>
+      <NewsCards  articles={newsArticles} activeArticle={activeArticle} />
+
     </div>
   );
 }
---
+++ src/Components/NewsCards/NewsCards.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import NewsCard from '../NewsCard/NewsCard';
+import { Grid , Grow  } from '@material-ui/core';
+import useStyles from './styles.js';
+
+import { Card , Tag , Divider } from 'antd';
+
+const { Meta } = Card;
+
+const infoCards = [
+    { color: '#00838f', title: 'Latest News', text: 'Give me the latest news' },
+    { color: '#1565c0', title: 'News by Categories', info: 'Business, Entertainment, General, Health, Science, Sports, Technology', text: 'Give me the latest Technology news' },
+    { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump', text: 'What\'s up with PlayStation 5' },
+    { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News', text: 'Give me the news from CNN' },
+  ];
+
+const colors = ['magenta','green','red','cyan','volcano','blue','orange','gold','lime',]
+const NewsCards = ({ articles , activeArticle }) => {
+    const classes = useStyles();
+
+    if(!articles.length) {
+        return (
+            <Grow in>
+                <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                    {infoCards.map((infoCard,index) => (
+                        <Grid key={index} item xs={12} sm={6} md={4} lg={3} className = {classes.infoCard}>
+                            <Card
+                                hoverable
+                                style={{ width: '100%' , height:'35vh' }}
+                                key={index}
+                            >
+                                <Meta title={infoCard.title} description={infoCard.text} />
+                                {infoCard.info?(<Divider orientation="left">{infoCard.title.split(' ')[2]}</Divider>):null}
+                                <div>
+                                    {infoCard.info?.split(',').map((ex,i) => (<Tag key={i} color={colors[i]}>{ex}</Tag>))}
+                                </div>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grow>
+        )
+    }
+    return (
+
+        <Grow in>
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                
+                    {articles.map((article,i) => (
+                        <Grid key={i} item xs={12} sm={6} md={4} lg={3} style = {{display:'flex'}}>
+                            <NewsCard article={article} i = {i} activeArticle={activeArticle} />
+                        </Grid>
+                    ))}
+                
+            </Grid>
+        </Grow>
+    )
+}
+
+export default NewsCards
